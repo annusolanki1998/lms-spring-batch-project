5a166e7d0f2698fccc336ce88fadb292e2752ab1
Annu <annusinghraj1998@gmail.com>
2022-08-25 10:42:44 +0530

    Created: Spring batch project setup done

---
+++ src/main/java/com/bridgelabz/lmsspringbatchproject/LmsSpringBatchProjectApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.lmsspringbatchproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsSpringBatchProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LmsSpringBatchProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bridgelabz/lmsspringbatchproject/LmsSpringBatchProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.lmsspringbatchproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsSpringBatchProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
